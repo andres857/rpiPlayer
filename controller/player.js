f8998db3a6e5d1b349b9006046029086e3d05206
andres857 <andres857@gmail.com>
2020-11-24 11:01:21 -0500

    work player

--- controller/player.js
+++ controller/player.js
@@ -1,7 +1,5 @@
 const execSync = require('child_process').execSync;
 
-let channel = 'https://www.youtube.com/watch?v=8j741TUIET0';
-
+let  channel = 'https://www.youtube.com/watch?v=FWyiKvPg1oo';
 code = execSync(`vlc -I dummy ${channel}, --fullscreen --video-on-top --no-video-title-show`);
 console.log(code);
-
