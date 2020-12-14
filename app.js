e7795207c9216c5be2a04c22b1e6496b33efe935
andres <andres@192.168.0.12>
2020-12-14 18:18:04 -0500

    delete connection to database

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const {client, topics} = require('./broker.js');
 const {player} = require ('./controller/player');
 const pub = require ('./publications');
 const sub = require ('./subscriptions')
-const db = require('./db');
+// const db = require('./db');
 
 
 
 async function main(){
-  await db('mongodb+srv://desarrollo:xSaTdGjM2AIWvVDJ@iptv.ywncf.mongodb.net/players?retryWrites=true&w=majority');
+  // await db('mongodb+srv://desarrollo:xSaTdGjM2AIWvVDJ@iptv.ywncf.mongodb.net/players?retryWrites=true&w=majority');
 
   player.launch(err => {
     if(err) return console.error(err.message);
